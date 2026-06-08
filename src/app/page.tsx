38ca2206077fd004c89b3163d3d769caf4f929cb
Laila <nurlailatulhidayah45@gmail.com>
2026-06-08 09:42:35 +0700

    feat: integrate supabase client end get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Service from "@/components/Service";
-import Portfolio from "@/components/Portfolio";
-import Process from "@/components/Process";
-import Testimonial from "@/components/Testimonial";
+"use client";
+
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
+import Hero from "@/components/sections/Hero";
+import About from "@/components/sections/About";
+import Service from "@/components/sections/Service";
+import Portfolio from "@/components/sections/portfolio/Portfolio";
+import Process from "@/components/sections/Process";
+import Testimonial from "@/components/sections/Testimonial";
+import Impact from "@/components/sections/Impact";
+import Cta from "@/components/sections/Cta";
 
 export default function Home() {
+  const getData = async () => {
+    const { data, error } = await supabase.from("hero").select("*");
+
+    console.log("DATA:", data);
+    console.log("ERROR:", error);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <Hero />
@@ -14,6 +31,8 @@ export default function Home() {
       <Portfolio />
       <Process />
       <Testimonial />
+      <Impact />
+      <Cta/>
     </main>
   );
 }
