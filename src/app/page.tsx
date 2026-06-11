01cfb22e7bdf6c4585973c16048a4b82e78e02ee
Laila <nurlailatulhidayah45@gmail.com>
2026-06-11 11:56:03 +0700

    feat: delete test get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,14 @@
-"use client";
 
-import { useEffect } from "react";
-import { supabase } from "@/lib/supabase";
-import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Service from "@/components/sections/Service";
+import Hero from "@/components/sections/hero/Hero";
+import About from "@/components/sections/about/About";
+import Service from "@/components/sections/service/Service";
 import Portfolio from "@/components/sections/portfolio/Portfolio";
-import Process from "@/components/sections/Process";
-import Testimonial from "@/components/sections/Testimonial";
-import Impact from "@/components/sections/Impact";
-import Cta from "@/components/sections/Cta";
+import Process from "@/components/sections/process/Process";
+import Testimonial from "@/components/sections/testimonial/Testimonial";
+import Impact from "@/components/sections/impact/Impact";
+import Cta from "@/components/sections/cta/Cta";
 
 export default function Home() {
-  const getData = async () => {
-    const { data, error } = await supabase.from("hero").select("*");
-
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <main>
@@ -32,7 +19,7 @@ export default function Home() {
       <Process />
       <Testimonial />
       <Impact />
-      <Cta/>
+      <Cta />
     </main>
   );
 }
