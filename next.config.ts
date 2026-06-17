5b3b20e4d0dbc29bdc86b399fadddaa7d8237e1e
Laila <nurlailatulhidayah45@gmail.com>
2026-06-17 08:45:31 +0700

    feat: image access

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kohsmiskubkxvvojxqso.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
